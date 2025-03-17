b366c0ae571e7f3473bce9e41fe44d69281ea42e
Daniel Pina <152346590+MrDumbell@users.noreply.github.com>
2025-03-17 16:04:57 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+
+public class main
+{
+    static void Main(string[] args)
+    {
+        string[] nomes = new string[5];
+        
+        Console.WriteLine("Introduza 5 nomes :");
+
+        for (int i = 0; i < nomes.Length; i++)
+        {
+            nomes[i] = Console.ReadLine();
+        }
+         
+        Array.Sort(nomes);
+        
+        Console.WriteLine(Environment.NewLine);
+        Console.WriteLine("Numeros por ordem: ");
+    
+        foreach (var nome in nomes)
+        {
+            
+            Console.WriteLine(nome);
+        }
+        
+    }
+}
